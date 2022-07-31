b3460dde45de5a92e00364827166bcd972d5b479
TAKAHIRO ITO <takahiro.ito@vacancorp.com>
2022-08-01 06:49:27 +0900

    feat: slot

---
+++ Tutorial27_slot/src/NinjaUsers.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+   <h1>List of ninjas</h1>
+   <ul>
+    <li v-for="(value, key) in NinjaUsers" :key="key">{{ value }}</li> 
+   </ul>
+ </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      NinjaUsers: ['Yoshi', 'Mario', 'Ryu']
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1{
+  color: green 
+}
+
+</style>
