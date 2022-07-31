8c612cf2583e24c61bda050e99d9bbb56c344ba9
TAKAHIRO ITO <takahiro.ito@vacancorp.com>
2022-08-01 03:09:49 +0900

    feat: add Vue components

---
+++ Tutorial15/app.js
@@ -0,0 +1,25 @@
+var data = {
+  name: 'Yoshi'
+}
+
+Vue.component('greeting',{
+ template: '<p>Hey there, I am {{ name }} . <button v-on:click="changeName">Chnge name</button></p>', 
+ data: function(){
+  return {
+    name: 'Yoshi'
+   }
+  },
+  methods: {
+    changeName: function(){
+      this.name = 'Mario';
+    }
+  }
+});
+
+var one = new Vue({
+  el: '#vue-app-one',
+});
+
+var two = new Vue({
+  el: '#vue-app-two',
+});
