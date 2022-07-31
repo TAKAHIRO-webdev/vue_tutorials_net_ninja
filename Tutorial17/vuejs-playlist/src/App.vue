6e2efd7f3c982ea2277e238ab9257d999770826d
TAKAHIRO ITO <takahiro.ito@vacancorp.com>
2022-08-01 05:06:40 +0900

    feat: add header, footer, body

--- Tutorial17/vuejs-playlist/src/App.vue
+++ Tutorial17/vuejs-playlist/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <NinjaUsers></NinjaUsers>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import NinjaUsers from "./NinjaUsers.vue";
+import MyHeader from "./components/MyHeader.vue";
+import MyFooter from "./components/MyFooter.vue";
+import MyNinjas from "./components/MyNinjas.vue";
 
 export default {
     components: {
-      'NinjaUsers': NinjaUsers
+      'app-header': MyHeader, 
+      'app-footer': MyFooter,
+      'app-ninjas': MyNinjas
     },
     data() {
         return {
