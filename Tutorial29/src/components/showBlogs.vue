9bb55421e175bc6fb75df3c9168661ce22efd07a
TAKAHIRO ITO <takahiro.ito@vacancorp.com>
2022-08-01 17:40:53 +0900

    feat: add Route Parameters

--- Tutorial29/src/components/showBlogs.vue
+++ Tutorial29/src/components/showBlogs.vue
@@ -3,7 +3,7 @@
    <h1>All Blog Articles</h1> 
    <input type="text" v-model="search" placeholder="search blogs"/>
    <div v-for="(blog, key) in filteredBlogs" :key="key" class="single-blog">
-     <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+    <router-link v-bind:to="'/blog/' + blog.id"></router-link> <h2>{{ blog.title | to-uppercase }}</h2>
      <article>{{ blog.body | snippet }}</article>
    </div>
   </div>
@@ -31,7 +31,7 @@ export default {
     },
     created(){
       this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-        this.blogs = data.body.slice(0,10);
+        this.blog = data.body.slice(0,10);
       });
     },
     computed: {
