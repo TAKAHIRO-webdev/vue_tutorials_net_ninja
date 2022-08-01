35008119f359c9d2cf749d875585ca56d4789025
TAKAHIRO ITO <takahiro.ito@vacancorp.com>
2022-08-01 18:00:39 +0900

    feat: add Firebase

--- Tutorial29/src/components/addBlog.vue
+++ Tutorial29/src/components/addBlog.vue
@@ -56,11 +56,7 @@ export default {
     },
     methods: {
       post: function(){
-        this.$http.post('https://jsonplaceholder.typicode.com/posts', {
-          title: this.blog.title,
-          body: this.blog.content,
-          userId: 1
-        }).then(function(data){
+        this.$http.post('https://taka-vue-ninja-20220801-default-rtdb.firebaseio.com/posts.json', this.blog).then(function(data){
           console.log(data);
           this.submitted = true;
         });
