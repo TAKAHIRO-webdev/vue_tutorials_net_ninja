5e275e847f12b4bc41a2bb2a72c385850fbaac7a
TAKAHIRO ITO <takahiro.ito@vacancorp.com>
2022-08-01 17:24:15 +0900

    feat: Added Route Links

--- Tutorial29/src/App.vue
+++ Tutorial29/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
@@ -9,12 +10,14 @@
 // import showBlogs from "./components/showBlogs.vue";
 // import listBlogs from "./components/listBlogs.vue";
 // import listBlogs from "./components/listBlogs.vue";
+import myHeader from "./components/myHeader.vue";
 
 export default {
   components: {
     // 'add-blog':addBlog,
     // 'show-blogs':showBlogs,
     // 'list-blogs':listBlogs
+    'app-header': myHeader
   },
     data() {
         return {
